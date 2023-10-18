fa6f161eb7279c3f23ab8103e86891f4e9b1a412
st20025 <st20025tm@gm.ibaraki-ct.ac.jp>
2023-10-18 11:36:41 +0900

    First commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">← Back to home</Link>
+      </h2>
+    </Layout>
+  );
+}
+
+/*const FirstPost = () =>{
+	return <h1>First Post</h1>;
+}
+
+export default FirstPost
+*/
